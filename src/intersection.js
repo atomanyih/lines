9c54f2fa183a77831763c304e53e2103b21111e0
August <augzodia@gmail.com>
2018-03-01 17:51:38 -0800

    add circle intersection calculator

---
+++ src/intersection.js
@@ -0,0 +1,19 @@
+// https://math.stackexchange.com/questions/228841/how-do-i-calculate-the-intersections-of-a-straight-line-and-a-circle
+// A = m * m + 1
+// B = m * c - m * q - p
+// C = q * q - r * r + p * p - 2 * c * q + c * c
+// x = (-B + Math.sqrt(B * B - 4 * A * C)) / (2 * A);
+// y = m * x + c;
+
+
+const findIntersection = ({c, m, cy, cx, r}) => {
+  const A = m * m + 1;
+  const B = m * c - m * cy - cx;
+  const C = cy * cy - r * r + cx * cx - 2 * c * cy + c * c;
+  const x = (-B + Math.sqrt(B * B - 4 * A * C)) / (2 * A);
+  const y = m * x + c;
+
+  return [x, y]
+};
+
+export default findIntersection;
